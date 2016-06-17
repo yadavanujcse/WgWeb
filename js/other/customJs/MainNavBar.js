19d881ab5c3c33f1beaf51bcb0c26510a763ffef
yadavanujcse <yadavanujcse@gmail.com>
2016-06-18 04:33:39 +0530

    adding all the files from JetBrains IDE

---
+++ js/other/customJs/MainNavBar.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $('.linkLocation').on('click',function(){
+        $('#userLocation').val(($(this).text().split(" ")[0]).trim());
+    });
+    $('.wgServices').on('click',function(){
+        $('#userServiceSelection').val($(this).text().trim());
+    });
+    $('#receiver').on('mouseenter mouseleave', function(e) {
+        if($('#selectUserLocation').attr('aria-expanded') && $('#selectUserLocation').parent().hasClass("open")){
+            $('#selectUserLocation').attr('aria-expanded','false');
+            $('#selectUserLocation').parent().removeClass('open');
+        }
+        $('#initiator').trigger(e.type);
+        if(e.type=="mouseleave"){
+            $('#receiver').blur();
+        }
+    });
+    $('#receiver').on('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    $('#userLocation').on('mouseenter click', function(e) {
+        e.preventDefault();
+
+        if(!($('#selectUserLocation').attr('aria-expanded') && $('#selectUserLocation').parent().hasClass("open"))){
+
+            $('#selectUserLocation').trigger('click');
+        }
+    });
+
+    $('#selectUserLocation').on('mouseenter', function(e) {
+        e.preventDefault();
+        if(!($('#selectUserLocation').attr('aria-expanded') && $('#selectUserLocation').parent().hasClass("open"))){
+            e.preventDefault();
+            $('#selectUserLocation').trigger('click');
+        }
+
+
+    });
+
+    $('wgdummy').on('click',function(){
+        e.preventDefault();
+    });
+});
+
+
---
+++ js/other/customJs/buyPack.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+});
+
+$('#buyPackageModal').on('shown.bs.modal', function () {
+    var vTop = $("#step1").offset().top - $("#rowNext").offset().top + $("#step1").height()/2;
+    $('#progressBarBuyPack').css('top',Math.ceil(vTop));
+    var vLeft = $("#step1").offset().left - $('#buyButtonConatiner').offset().left + $("#step1").width()/2;
+    $('#progressBarBuyPack').css('left',Math.ceil(vLeft));
+    var vRight =  $("#step4").offset().left - $('#buyButtonConatiner').offset().left + $("#step4").width()/2
+    $('#progressBarBuyPack').css('width',Math.ceil(vRight-vLeft));
+    $('#progressBarBuyPack').css('display','block');
+    $('#progressBarBuyPack').css('position','relative');
+    $('#progressBarBuyPack').css('top','48px');
+
+});
+
+$('#step1').on('click',function(){
+   $('#progressBarBuyPack').attr('value',0);
+
+});
+$('#step2').on('click',function(){
+   $('#progressBarBuyPack').attr('value',33);
+});
+$('#step3').on('click',function(){
+   $('#progressBarBuyPack').attr('value',66);
+});
+$('#step4').on('click',function(){
+   $('#progressBarBuyPack').attr('value',100);
+});
