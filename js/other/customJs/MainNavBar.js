9713bf52724a5c2dacbe9cb4d3865a9969a54701
yadavanujcse <yadavanujcse@gmail.com>
2016-06-19 06:15:23 +0530

    functionality fixes related to search on navbar

--- js/other/customJs/MainNavBar.js
+++ js/other/customJs/MainNavBar.js
@@ -5,49 +5,7 @@ $(document).ready(function(){
     $('.wgServices').on('click',function(){
         $('#userServiceSelection').val($(this).text().trim());
     });
-    $('#receiver').on('mouseenter mouseleave click', function(e) {
-        if($('#selectUserLocation').attr('aria-expanded') && $('#selectUserLocation').parent().hasClass("open")){
-            $('#selectUserLocation').attr('aria-expanded','false');
-            $('#selectUserLocation').parent().removeClass('open');
-        }
-        $('#initiator').trigger(e.type);
 
-
-
-        if(e.type=="mouseleave"){
-            $('#receiver').blur();
-        }
-
-    });
-
-
-    $('#receiver').on('click', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-    });
-
-    $('#userLocation').on('mouseenter click', function(e) {
-        e.preventDefault();
-
-        if(!($('#selectUserLocation').attr('aria-expanded') && $('#selectUserLocation').parent().hasClass("open"))){
-
-            $('#selectUserLocation').trigger('click');
-        }
-    });
-
-    $('#selectUserLocation').on('mouseenter', function(e) {
-        e.preventDefault();
-        if(!($('#selectUserLocation').attr('aria-expanded') && $('#selectUserLocation').parent().hasClass("open"))){
-            e.preventDefault();
-            $('#selectUserLocation').trigger('click');
-        }
-
-
-    });
-
-    $('wgdummy').on('click',function(){
-        e.preventDefault();
-    });
 });
 
 
