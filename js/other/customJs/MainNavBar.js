2a016184f478a53b95c2ee4164ae8ec5c8edcb56
yadavanujcse <yadavanujcse@gmail.com>
2016-06-20 02:10:49 +0530

    Changes uptodate for search box

--- js/other/customJs/MainNavBar.js
+++ js/other/customJs/MainNavBar.js
@@ -1,9 +1,34 @@
 $(document).ready(function(){
     $('.linkLocation').on('click',function(){
         $('#userLocation').val(($(this).text().split(" ")[0]).trim());
+        if($('#userLocation').val()!=''){
+            $('#locationError').css("display","none");
+        }
     });
     $('.wgServices').on('click',function(){
         $('#userServiceSelection').val($(this).text().trim());
+        if($('#userLocation').val()==''){
+            $('#locationError').css("display","block");
+        }else{
+            $('#locationError').css("display","none");
+        }
+        if($('#userServiceSelection').val()!=''){
+            $('#serviceError').css("display","none");
+        }
+    });
+
+    $('#BookNow').on('click', function () {
+        if($('#userLocation').val()==''){
+            $('#locationError').css("display","block");
+        }else{
+            $('#locationError').css("display","none");
+        }
+        if($('#userServiceSelection').val()==''){
+            $('#serviceError').css("display","block");
+        }else{
+            $('#serviceError').css("display","none");
+        }
+
     });
 
 });
